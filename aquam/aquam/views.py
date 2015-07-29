e6b5989426c908ec3036b2e72111461e48618118
jerryer <learn3711@gmail.com>
2015-07-29 10:45:20 -0600

    change name AQUAM to VODA

--- aquam/aquam/views.py
+++ aquam/aquam/views.py
@@ -4,28 +4,28 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        "page_title": "AQUAM | Oilfield Water Management",
+        "page_title": "VODA | Oilfield Water Management",
     }
     return render(request, "index.html", context)
 
 
 def solutions(request):
     context = {
-        "page_title": "AQUAM | Solutions",
+        "page_title": "VODA | Solutions",
     }
     return render(request, "solutions.html", context)
 
 
 def geoanalytics(request):
     context = {
-        "page_title": "AQUAM | Geoanalytics",
+        "page_title": "VODA | Geoanalytics",
     }
     return render(request, "geoanalytics.html", context)
 
 
 def contact(request):
     context = {
-        "page_title": "AQUAM | Contact",
+        "page_title": "VODA | Contact",
     }
     return render(request, "contact.html", context)
 
